218378398b18b8845df1723f0b1bff4b6aac3d2d
lorix <foxred324@gmail.com>
2020-06-04 15:45:09 +0300

    fix spiral for 3x3

--- main.go
+++ main.go
@@ -11,25 +11,24 @@ type Point struct {
 
 func GoldenRatio (matrix [][]int) []int {
 	size := len(matrix)
-	start_point := Point{x : int(size/2), y: int(size/2) }
-	end_point := Point{x: 1, y: size }
 	direction := 0 												//current direction; 0=RIGHT, 1=DOWN, 2=LEFT, 3=UP
 	counter := 0
 	chain_size := 1
-	x := 0
-	y := 0
+	x := int((size / 2))
+	y := int((size / 2))
+	result := make([]int, 0)
 	fmt.Println("start point: ", start_point )
 	fmt.Println("end point: ", end_point )
 	for k := 1; k <= ( size - 1 ); k++ {
 		if k < (size - 1){
 			for j := 0; j < 2; j++ {
 				for i := 0; i < chain_size; i++ {
-					fmt.Printf("%v ", matrix[x][y])
+					result = append(result, matrix[x][y])
 					counter++
 					switch direction {
-						case 0: y = y + 1
+						case 0: y = y - 1
 						case 1: x = x + 1
-						case 2: y = y - 1
+						case 2: y = y + 1
 						case 3: x = x - 1
 					}
 				}
@@ -38,21 +37,21 @@ func GoldenRatio (matrix [][]int) []int {
 		} else {
 			for j := 0; j < 3; j++ {
 				for i := 0; i < chain_size; i++ {
-					fmt.Printf("%v ", matrix[x][y])
+					result = append(result, matrix[x][y])
 					counter++
 					switch direction {
-						case 0: y = y + 1
+						case 0: y = y - 1
 						case 1: x = x + 1
-						case 2: y = y - 1
+						case 2: y = y + 1
 						case 3: x = x - 1
 					}
 				}
 				direction = (direction+1)%4;
 			}
 		}
-        chain_size = chain_size + 1;
-    }
-	return make([]int, 0)
+		chain_size = chain_size + 1;
+	}
+	return result
 }
 
 
