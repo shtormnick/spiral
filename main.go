8da8f73d5c30eccddff496f8dbd4d51735ef2fde
lorix <foxred324@gmail.com>
2020-06-15 01:51:13 +0300

    fix spiral method

--- main.go
+++ main.go
@@ -1,68 +1,51 @@
 package main
 
 import (
+	"os"
 	"fmt"
 )
 
+const R int = 3 // capacity of row
+const C int = 3 // capacitu of column
 
+func GoldenRatio(matrix [][]int, i, j, m, n int) ([]int) { // i - index of first item ,j - index of first column, m - index of last index, n - index of last column
 
-func GoldenRatio (matrix [][]int) []int {
-	size := len(matrix)
-	direction := 0 												//current direction; 0=RIGHT, 1=DOWN, 2=LEFT, 3=UP
-	counter := 0
-	chain_size := 1
-	x := int((size / 2))
-	y := int((size / 2))
 	result := make([]int, 0)
-	for k := 1; k <= ( size - 1 ); k++ {
-		if k < (size - 1){
-			for j := 0; j < 2; j++ {
-				for i := 0; i < chain_size; i++ {
-					result = append(result, matrix[x][y])
-					counter++
-					switch direction {
-						case 0: y = y - 1
-						case 1: x = x + 1
-						case 2: y = y + 1
-						case 3: x = x - 1
-					}
-				}
-				direction = (direction+1)%4;
-			}
-		} else {
-			for j := 0; j < 3; j++ {
-				for i := 0; i < chain_size; i++ {
-					result = append(result, matrix[x][y])
-					counter++
-					switch direction {
-						case 0: y = y - 1
-						case 1: x = x + 1
-						case 2: y = y + 1
-						case 3: x = x - 1
-					}
-				}
-				direction = (direction+1)%4;
-			}
-		}
-		chain_size = chain_size + 1;
+	last_elem := matrix[1][1]
+	if i >= m || j >= n || m != n {
+		os.Exit(1)
 	}
-	return result
-}
 
-func reverse(numbers []int) []int {
-	newNumbers := make([]int, 0, len(numbers))
-	for i := len(numbers)-1; i >= 0; i-- {
-		newNumbers = append(newNumbers, numbers[i])
+	for p := i; p < n; p++ {
+		result = append(result, matrix[i][p])
 	}
-	return newNumbers
+
+	for p := i + 1; p < m; p++ {
+		result = append(result, matrix[p][n-1])
+	}
+
+	if (m - 1) != i {
+		for p := n - 2; p >= j; p-- {
+			result = append(result, matrix[m-1][p])
+		}
+	}
+
+	if (n - 1) != j {
+		for p := m - 2; p > i; p-- {
+			result = append(result, matrix[p][j])
+		}
+	}
+
+	result = append(result, last_elem)
+	return result  
 }
 
 func main() {
 	input := [][]int{
-		[]int{1,2,3},
-		[]int{4,5,6},
-		[]int{7,8,9},
+		[]int{1, 2, 3},
+		[]int{4, 5, 6},
+		[]int{7, 8, 9},
 	}
-	array := GoldenRatio(input)
-	fmt.Println(reverse(array))
+	fmt.Println(GoldenRatio(input, 0, 0, R, C))
+
 }
