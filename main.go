101481fd5190bc9f4d9120b1e6f9216cbe16b1b0
lorix <foxred324@gmail.com>
2020-06-15 23:43:01 +0300

    final fixes for spiral method

--- main.go
+++ main.go
@@ -1,51 +1,56 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	//"os"
 )
 
-const R int = 3 // capacity of row
-const C int = 3 // capacitu of column
+const R int = 4 // capacity of row
+const C int = 4 // capacitu of column
 
-func GoldenRatio(matrix [][]int, i, j, m, n int) ([]int) { // i - index of first item ,j - index of first column, m - index of last index, n - index of last column
-
+func GoldenRatio(matrix [][]int, m, n int) []int { // i - index of first item ,j - index of first column, m - index of last index, n - index of last column
+	var  k, l int = 0, 0
 	result := make([]int, 0)
-	last_elem := matrix[1][1]
-	if i >= m || j >= n || m != n {
-		os.Exit(1)
-	}
 
-	for p := i; p < n; p++ {
-		result = append(result, matrix[i][p])
-	}
+	for k < m && l < n {
 
-	for p := i + 1; p < m; p++ {
-		result = append(result, matrix[p][n-1])
-	}
+		for i := l; i < n; i++ {
+			result = append(result, matrix[k][i])
+		}
 
-	if (m - 1) != i {
-		for p := n - 2; p >= j; p-- {
-			result = append(result, matrix[m-1][p])
+		k++
+
+		for i := k; i < m; i++ {
+			result = append(result, matrix[i][n-1])
+		}
+
+		n--
+
+		if k < m {
+			for i := n - 1; i >= l; i-- {
+				result = append(result, matrix[m-1][i])
+			}
+			m--
+		}
+
+		if l < n {
+			for i := m - 1; i >= k; i-- {
+				result = append(result, matrix[i][l])
+			}
+			l++
 		}
 	}
 
-	if (n - 1) != j {
-		for p := m - 2; p > i; p-- {
-			result = append(result, matrix[p][j])
-		}
-	}
-
-	result = append(result, last_elem)
-	return result  
+	return result
 }
 
 func main() {
 	input := [][]int{
-		[]int{1, 2, 3},
-		[]int{4, 5, 6},
-		[]int{7, 8, 9},
+		[]int{1, 2, 3, 4},
+		[]int{4, 5, 6, 7},
+		[]int{7, 8, 9, 0},
+		[]int{7, 8, 9, 0},
 	}
-	fmt.Println(GoldenRatio(input, 0, 0, R, C))
+	fmt.Println(GoldenRatio(input, R, C))
 
 }
